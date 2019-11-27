4d42e686157ca7c997c839d2273af01a01b323bd
Hyginus Ukeh <ukeh.hyginus@techvibesltd.com>
2019-11-27 01:00:39 +0100

    Added font awesome package, created route pages and created TopNavigationBar Component

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const About = () => (
+    <React.Fragment>
+        <p>Hello About</p>
+    </React.Fragment>
+);
--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './Home';
+import {About} from  './About';
+import {DefaultNotFound} from  './DefaultNotFound';
+import {TopNavigationBar} from './components/TopNavigationBar';
 
 function App() {
   return (
-    <div>
-      <p>My App</p>
-    </div>
+    <React.Fragment>
+      <TopNavigationBar></TopNavigationBar>
+      <Router> 
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route component={DefaultNotFound}/>
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
