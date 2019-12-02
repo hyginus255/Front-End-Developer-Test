7a26b70662eb7690924be71a0989a4dc830d8c8a
Hyginus Ukeh <ukeh.hyginus@techvibesltd.com>
2019-12-02 10:59:02 +0100

    update on readme file and refactoring of BottomNavigationBar

--- src/components/bottomNavigation/BottomNavigationBar.js
+++ src/components/bottomNavigation/BottomNavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Container, Form, FormControl, Button, InputGroup, Row} from 'react-bootstrap';
+import {Container, Form, FormControl, Button, InputGroup, Nav} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import {Link} from "react-router-dom";
@@ -10,23 +10,21 @@ export const BottomNavigationBar = () => (
     <Styles>
          <Container fluid={true} >
             <Container>
-                <Row>
-                    <Link to="/tables/CL" className="nav-link">uefa</Link>
-                    <Link to="/tables/PL" className="nav-link">epl</Link>
-                    <Link to="/tables/WLC" className="nav-link">super cup</Link>
-                    <Link to="/" className="nav-link">Liga bbva</Link>
-                    <Link to="/" className="nav-link">world cup</Link>
-                    <Link to="/" className="nav-link">npl</Link>
-                    <Link to="/" className="nav-link">Features</Link>
-                    <Link to="/" className="nav-link">nation league</Link>
-                    <Link to="/" className="nav-link">archive</Link>
+                <Nav>
+                    <Nav.Link href="/tables/CL">uefa</Nav.Link>
+                    <Nav.Link href="/tables/PL">epl</Nav.Link>
+                    <Nav.Link href="/">Liga bbva</Nav.Link>
+                    <Nav.Link href="/tables/2120">super cup</Nav.Link>
+                    <Nav.Link href="/tables/WC">world cup</Nav.Link>
+                    <Nav.Link href="/tables/2191">npl</Nav.Link>
+                    <Nav.Link href="/tables/SPL">Premier League</Nav.Link>
                     <Form inline>
                         <InputGroup>
                             <FormControl type="text" placeholder="Search" />
                             <Button><FontAwesomeIcon icon={faSearch} /></Button>
                         </InputGroup>
                     </Form>
-                </Row>
+                </Nav>
             </Container>
          </Container>
     </Styles>
